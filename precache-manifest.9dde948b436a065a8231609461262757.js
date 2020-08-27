a7c5ad717514157db4e060737e0c8f1d57923d5a
Lorenze Jay <Lorenzejay@gmail.com>
2020-08-26 22:13:12 -0700

    Updates

---
+++ precache-manifest.9dde948b436a065a8231609461262757.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc9ddae7e39ce8766e567f520c36715",
+    "url": "/breaking-the-bad/index.html"
+  },
+  {
+    "revision": "a41e61d9ecfb06217331",
+    "url": "/breaking-the-bad/static/css/main.ced81294.chunk.css"
+  },
+  {
+    "revision": "eb0e5030460bc7d2f256",
+    "url": "/breaking-the-bad/static/js/2.791a7e21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-the-bad/static/js/2.791a7e21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41e61d9ecfb06217331",
+    "url": "/breaking-the-bad/static/js/main.78f9f780.chunk.js"
+  },
+  {
+    "revision": "5cc6819b56f120d83cdb",
+    "url": "/breaking-the-bad/static/js/runtime-main.5124565f.js"
+  },
+  {
+    "revision": "5c9ea4a73eb8a27315776732509f33e5",
+    "url": "/breaking-the-bad/static/media/breaking-bad-bg-img.5c9ea4a7.jpg"
+  }
+]);
