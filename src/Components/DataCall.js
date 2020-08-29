7b9301f0e59e74975ac3f8a55802c703304adf75
Lorenze Jay <Lorenzejay@gmail.com>
2020-08-29 06:59:28 -0700

    Lorenze Edit pt.2

--- src/Components/DataCall.js
+++ src/Components/DataCall.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import axios from "axios";
 import CharacterCard from "./CharacterCard";
@@ -46,32 +45,3 @@ const DataCall = () => {
 // }
 
 export default DataCall;
-=======
-import React, { Component } from 'react';
-import axios from 'axios';
-
-class DataCall extends Component{
-  constructor(props){
-    super(props);
-    this.state = {}
-  }
-
-  async getInfo(){
-    const res = await axios.get('http://localhost:3008/characters');
-    const characters = res.data;
-    console.log(characters); // Here is the array of Characters from the Backend.
-  }
-
-  componentDidMount(){
-    this.getInfo();
-  }
-  
-  render(){
-    return(
-      <div></div>
-    );
-  }
-}
-
-export default DataCall;
->>>>>>> bf6c7528574908d123b11b9459da2b54b2b950ad
